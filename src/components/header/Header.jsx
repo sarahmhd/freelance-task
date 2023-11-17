1c37c8c5cfa2af85795115f17a74d0db7e91bcd5
sarahmhd <sarahmhd213199@gmail.com>
2023-11-17 23:54:18 +0200

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,16 +2,17 @@ import { HashLink } from "react-router-hash-link";
 import { Link } from "react-router-dom";
 import React from "react";
 import logo from "../../assets/imgs/Logo.svg";
+import styles from "./header.module.css";
 
 const Header = () => {
   return (
-    <nav class="navbar navbar-expand-lg">
-      <div class="container">
-        <Link class="navbar-brand" to="/">
+    <nav className={`navbar navbar-expand-lg ${styles.navbar}`}>
+      <div className="container">
+        <Link className="navbar-brand" to="/">
           <img src={logo} />
         </Link>
         <button
-          class="navbar-toggler"
+          className={`navbar-toggler ${styles.toggler}`}
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
@@ -19,47 +20,71 @@ const Header = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav m-auto">
-            <li class="nav-item">
-              <HashLink class="nav-link active" smooth to="/about-us/#section1">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav m-auto">
+            <li className="nav-item">
+              <HashLink
+                className="nav-link active"
+                smooth
+                to="/about-us/#section1"
+              >
                 About us
               </HashLink>
             </li>
-            <li class="nav-item">
-              <HashLink class="nav-link active" smooth to="/about-us/#section1">
+            <li className="nav-item">
+              <HashLink
+                className="nav-link active"
+                smooth
+                to="/about-us/#section1"
+              >
                 Services
               </HashLink>
             </li>
-            <li class="nav-item">
-              <HashLink class="nav-link active" smooth to="/about-us/#section1">
+            <li className="nav-item">
+              <HashLink
+                className="nav-link active"
+                smooth
+                to="/about-us/#section1"
+              >
                 Case Studies
               </HashLink>
             </li>
-            <li class="nav-item">
-              <HashLink class="nav-link active" smooth to="/about-us/#section1">
+            <li className="nav-item">
+              <HashLink
+                className="nav-link active"
+                smooth
+                to="/about-us/#section1"
+              >
                 Blog
               </HashLink>
             </li>
-            <li class="nav-item">
-              <HashLink class="nav-link active" smooth to="/about-us/#section1">
+            <li className="nav-item">
+              <HashLink
+                className="nav-link active"
+                smooth
+                to="/about-us/#section1"
+              >
                 How it Works
               </HashLink>
             </li>
-            <li class="nav-item">
-              <HashLink class="nav-link active" smooth to="/about-us/#section1">
+            <li className="nav-item">
+              <HashLink
+                className="nav-link active"
+                smooth
+                to="/about-us/#section1"
+              >
                 Hire
               </HashLink>
             </li>
-            {/* <li class="nav-item">
-              <a class="nav-link" href="#">
+            {/* <li className="nav-item">
+              <a className="nav-link" href="#">
                 Pricing
               </a>
             </li> */}
           </ul>
-          <button class="btn">Contact us</button>
+          <button className={`btn ${styles.contact_btn}`}>Contact us</button>
         </div>
       </div>
     </nav>
