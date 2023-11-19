e04a63bfdfb1723b30ca4812db87bfb9054a182b
sarahmhd <sarahmhd213199@gmail.com>
2023-11-19 14:32:04 +0200

    hiring section

--- src/components/hiring/Hiring.jsx
+++ src/components/hiring/Hiring.jsx
@@ -1,7 +1,18 @@
+import "./hiring.css";
+
 import React from "react";
 
 const Hiring = () => {
-  return <div>Hiring</div>;
+  return (
+    <div className="hiring" id="hire">
+      <div className="container">
+        <div className="hiring-content d-flex justify-content-between align-items-center">
+          <h2>Hire the best developers and designers around!</h2>
+          <button>Hire Top Developers</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hiring;
